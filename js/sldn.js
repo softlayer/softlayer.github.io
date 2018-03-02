26f88f70848d3569f746e5c2b7097b76bd7efd2d
Christopher Gallo <cgallo@us.ibm.com>
2018-03-01 18:45:56 -0600

    added sldn reference articles

---
+++ js/sldn.js
@@ -0,0 +1,18 @@
+function titleSearch(inputId='edit-combine', divId='method-div', elementClass='method-row') {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById(inputId);
+    filter = input.value.toUpperCase();
+    methods = document.getElementById(divId);
+    li = methods.getElementsByClassName(elementClass);
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
