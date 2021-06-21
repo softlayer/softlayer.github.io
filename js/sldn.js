2a60961c9d8b12beece4c4a13bb3f476286a4486
SLDN <sldndeveloperrelations@wwpdl.vnet.ibm.com>
2021-06-21 21:23:17 +0000

    Mon Jun 21 21:23:16 UTC 2021 Release

--- js/sldn.js
+++ js/sldn.js
@@ -9,7 +9,8 @@ function titleSearch(inputId='edit-combine', divId='method-div', elementClass='m
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        search_term = "SOFTLAYER_"+a.innerHTML.trim().toUpperCase();
+        if (search_term.indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
